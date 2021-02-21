13d324eb42ad487d09c68c1bf577ba9cd5c966cb
Cloud User <ec2-user@ip-172-31-90-23.ec2.internal>
2021-02-21 17:15:53 +0000

    Executable running svm-train && svm-predict

---
+++ svm_test_predict_exec.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+#################################################
+# Run svm-train then svm-predict
+#################################################
+
+#################################################
+# 1 arg: <path_to_input_parameter>/train0.svm
+# 2 arg: <path_to_output_parameter>/svm_outputs/train/train0.model
+# 2 arg: serves also as input to svm-predict
+# 3 arg: <path_to_input_parameter>/in_predict_tiny/test0.svm 
+# 4 arg: <path_to_output_parameter>/svm_outputs/predict/prediction0.out
+#################################################
+
+svm-train -c 2 -g 0.5 -m 2000 $1 $2 && \
+svm-predict $3 $2 $4
