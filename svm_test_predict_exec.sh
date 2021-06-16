94c9b56041217e7225d826d227fe831c09aa8109
Cloud User <centos@ip-172-31-1-180.ec2.internal>
2021-06-16 17:32:45 +0000

    adjusted job instances to 6

--- svm_test_predict_exec.sh
+++ svm_test_predict_exec.sh
@@ -10,7 +10,8 @@
 # 3 arg: <path_to_input_parameter>/in_predict_tiny/test0.svm 
 # 4 arg: <path_to_output_parameter>/svm_outputs/predict/prediction0.out
 #################################################
-
-svm-train -c 2 -g 0.5 -m 2000 $1 $2 && \
+# Measure time of each jobinstance
+SECONDS=0 
+svm-train -c 2 -g 0.5 -m 1024 $1 $2 && \
 svm-predict $3 $2 $4
-
+echo "Duration" $SECONDS
